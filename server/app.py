d563a9c442f354139bda143a768a28524e7fabb8
KillianCARRAT <killian.carrat@etu.univ-orleans.fr>
2025-03-25 17:59:42 +0100

    reglage

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ def mkpath(p):
     return os.path.normpath(os.path.join(os.path.dirname(__file__), p))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'sqlite:///'+mkpath('../questionnaire.db')
+    'sqlite:///'+mkpath('../server.db')
 )
 db = SQLAlchemy(app)
 
