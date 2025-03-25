a31701bcf5030ad21f72bb4855a92bfef4a60e8d
KillianCARRAT <killian.carrat@etu.univ-orleans.fr>
2025-03-25 17:43:58 +0100

    Base

--- projetquiz/src/App.vue
+++ projetquiz/src/App.vue
@@ -1,30 +1,94 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import TodoItem from './components/TodoItem.vue';
+
+let data = {
+  todos: [
+    { id:0, text: 'Faire les courses', checked: true },
+    { id:1, text: 'Apprendre REST', checked: false }
+  ],
+  title: 'Mes tâches',
+  newItem: ''
+};
+
+export default {
+  data() {
+    return data;
+  },
+  methods: {
+    addItem: function () {
+      let text = this.newItem.trim();
+      if (text) {
+        this.todos.push({
+          id: this.todos.length,
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
+    },
+    removeItem: function ($event) {
+
+      for (let i = 0; i < this.todos.length; i++) {
+        if ($event.id == this.todos[i].id) {
+          this.todos.splice(i, 1);
+          break;
+        }
+      }
+    },
+    updateItem: function($event){
+      for (let i = 0; i < this.todos.length; i++) {
+        if ($event.id == this.todos[i].id) {
+          this.todos[i].text = $event.text;
+          break;}
+    }}
+  },
+  mounted() {
+    fetch('http://127.0.0.1:5000/todo/api/v1.0/tasks')
+      .then(response => response.json())
+      .then(data => {
+        this.todos = data.tasks;
+      });
+  },
+  components: {
+    TodoItem
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+    crossorigin="anonymous"
+  >
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <ol>
+      <TodoItem
+      v-for="todo in todos"
+      :todo="todo"
+      @remove="removeItem"
+      @update="updateItem"
+      />
+    </ol>
+    <div class="input-group">
+      <input
+        v-model="newItem"
+        @keyup.enter="addItem"
+        placeholder="Ajouter une tache à la liste"
+        type="text"
+        class="form-control"
+      >
+      <span class="input-group-btn">
+        <button
+          @click="addItem"
+          class="btn btn-default"
+          type="button"
+        >
+          Ajouter
+        </button>
+      </span>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
